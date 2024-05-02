3d56d72530493e5dcf3a255877700d2410dfe08e
adriandcoding <theamr91@gmail.com>
2024-05-02 16:13:19 +0200

    implementando ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,30 @@
-import "./style.css";
+const Numero_actual: any = document.querySelector(".numero-turno");
+const Turno_anterior: any = document.querySelector(".boton-anterior");
+const Resetear_botón: any = document.querySelector(".boton-reset");
+const Boton_siguiente: any = document.querySelector(".boton-siguiente");
+const Introducir_numero: any = document.querySelector("#introducir_turno");
+const Boton_enviar: any = document.querySelector(".boton-enviar");
+let turnoActual = 1;
 
-console.log("Hello Typescript!");
+function resetear() {
+  turnoActual = 1;
+  Numero_actual.innerHTML = turnoActual.toString().padStart(2, "0");
+}
+function turnoAnterior() {
+  if (turnoActual > 1) {
+    turnoActual--;
+    Numero_actual.innerHTML = turnoActual.toString().padStart(2, "0");
+  }
+}
+function turnoSiguiente() {
+  turnoActual++;
+  Numero_actual.innerHTML = turnoActual.toString().padStart(2, "0");
+}
+function escribirTurno() {
+  Numero_actual.innerHTML = Introducir_numero.value;
+}
+
+Turno_anterior.addEventListener("click", turnoAnterior);
+Resetear_botón.addEventListener("click", resetear);
+Boton_siguiente.addEventListener("click", turnoSiguiente);
+Boton_enviar.addEventListener("click", escribirTurno);
