8ac63d74370d6f6691a5665046be1eeba9996368
adriandcoding <theamr91@gmail.com>
2024-05-02 19:17:05 +0200

    cambios main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,49 @@
-const Numero_actual: any = document.querySelector(".numero-turno");
-const Turno_anterior: any = document.querySelector(".boton-anterior");
-const Resetear_botón: any = document.querySelector(".boton-reset");
-const Boton_siguiente: any = document.querySelector(".boton-siguiente");
-const Introducir_numero: any = document.querySelector("#introducir_turno");
-const Boton_enviar: any = document.querySelector(".boton-enviar");
-let turnoActual = 1;
+//leer los inputs y botones
 
-function resetear() {
+const Numero_actual:any= document.querySelector(".numero-turno");
+const Turno_anterior = document.querySelector(".boton-anterior");
+const Resetear_botón = document.querySelector(".boton-reset");
+const Boton_siguiente = document.querySelector(".boton-siguiente");
+const Introducir_numero:any = document.querySelector("#introducir_turno");
+const Boton_enviar = document.querySelector(".boton-enviar");
+
+let turnoActual =1
+if (Numero_actual != null && Numero_actual !== undefined) {
+  Numero_actual.innerHTML = turnoActual.toString().padStart(2, "0");
+}
+
+// funciones
+function resetear():void{
   turnoActual = 1;
   Numero_actual.innerHTML = turnoActual.toString().padStart(2, "0");
 }
-function turnoAnterior(){
+function turnoAnterior():void{
   if (turnoActual > 1) {
     turnoActual--;
     Numero_actual.innerHTML = turnoActual.toString().padStart(2, "0");
   }
 }
-function turnoSiguiente() {
+function turnoSiguiente():void{
   turnoActual++;
   Numero_actual.innerHTML = turnoActual.toString().padStart(2, "0");
 }
-const escribirTurno=() =>
+const escribirTurno=():number=>
   Numero_actual.innerHTML = Introducir_numero.value;
 
+// aplicando funciones
 
-Turno_anterior.addEventListener("click", turnoAnterior);
-Resetear_botón.addEventListener("click", resetear);
-Boton_siguiente.addEventListener("click", turnoSiguiente);
-Boton_enviar.addEventListener("click", escribirTurno);
+if (Turno_anterior != null && Turno_anterior != undefined) {
+  Turno_anterior.addEventListener("click", turnoAnterior);
+}
+
+if (Boton_siguiente != null && Boton_siguiente != undefined) {
+  Boton_siguiente.addEventListener("click", turnoSiguiente);
+}
+if (Resetear_botón != null && Resetear_botón != undefined) {
+  Resetear_botón.addEventListener("click", resetear);
+}
+if (Boton_enviar != null && Boton_enviar != undefined) {
+  Boton_enviar.addEventListener("click", escribirTurno);
+}
+
+
